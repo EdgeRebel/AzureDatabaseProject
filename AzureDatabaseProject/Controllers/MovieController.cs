9fbca58a5859ce0aa1ef59937401263b318e4007
jiocloud <4ygyiioloj@cloud-mail.top>
2021-03-29 10:48:48 +0530

    Initial commit

---
+++ AzureDatabaseProject/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureDatabaseProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureDatabaseProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private ShowContext _context;
+
+        public MovieController(ShowContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Movie>> Get()
+        {
+            var result = await _context.Movies.ToListAsync();
+            return result;
+        }
+    }
+}
---
+++ AzureDatabaseProject/Models/ShowContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureDatabaseProject.Models
+{
+    public class ShowContext : DbContext
+    {
+        public ShowContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie { Id = 101, Name = "X-Men", Description = "Sci-Fi", Duration = 123.5f});
+        }
+    }
+}
